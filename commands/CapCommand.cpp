6d571b5544c7400c1c75998dfa84cd127f33234c
Husrev Akbas <huakbas@c3r9p6.42vienna.com>
2025-11-03 16:23:29 +0100

    refactor: sleep removed from cap command password check

--- commands/CapCommand.cpp
+++ commands/CapCommand.cpp
@@ -46,7 +46,6 @@ void    CapCommand::response(Client &client, Server &server)
         {
             response = buildNumericReply(server, client, ERR_PASSWDMISMATCH, "Wrong password mate!");
             server.sendResponse(client, response);
-            sleep(1); // TODO wait enough until client gets the response
             server.removeClient(&client);
             return ;
         }
